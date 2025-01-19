6428f0762e5729b74b172030a16235f2971682e9
hthoairdrop <htho.gradient1@gmail.com>
2025-01-19 19:33:17 +0700

    de

--- openloop/autoreff.js
+++ openloop/autoreff.js
@@ -83,12 +83,47 @@ async function manageMailAndRegister() {
             : 'ol2d3a6bea';
 
         logger(`Register Using Referral code: ${referralCode}`, 'info');
+        const accountStr = 
+`leena.nori818407@motesh.com,Huutho06.vn
+aradhana.chaudhari80173@fosaga.com,Huutho06.vn
+rachana.rama251288@motesh.com,Huutho06.vn
+abhimanyu.dayal773541@givima.com,Huutho06.vn
+nachiket.krishnan383735@fosaga.com,Huutho06.vn
+bhanumati.munshi200902@mobesu.com,Huutho06.vn
+george.lall391994@givima.com,Huutho06.vn
+jhalak.konda517470@fosaga.com,Huutho06.vn
+bhavika.deep966722@gutade.com,Huutho06.vn
+kashvi.nigam94160@motesh.com,Huutho06.vn
+kavya.mammen160963@motesh.com,Huutho06.vn
+idika.sarma959930@givima.com,Huutho06.vn
+pavani.bala200742@pogite.com,Huutho06.vn
+bhavna.rajagopalan362683@temipo.com,Huutho06.vn
+guneet.deo419843@gutade.com,Huutho06.vn
+netra.mane997780@fadoso.com,Huutho06.vn
+kashvi.srinivasan194616@pogite.com,Huutho06.vn
+naveen.kala572422@pogite.com,Huutho06.vn
+krishna.ravel862476@pogite.com,Huutho06.vn
+rachana.dhar248116@fosaga.com,Huutho06.vn
+prisha.shenoy828794@temipo.com,Huutho06.vn
+kiaan.dash314566@mobesu.com,Huutho06.vn
+omya.nayar125558@gutade.com,Huutho06.vn
+anya.gokhale863147@givima.com,Huutho06.vn
+caleb.puri337222@givima.com,Huutho06.vn
+bhavna.dhar464028@pogite.com,Huutho06.vn
+nicholas.kothari565645@fozina.com,Huutho06.vn
+caleb.pandey164563@fadoso.com,Huutho06.vn
+nicholas.sekhon550567@motesh.com,Huutho06.vn
+chasmum.iyer422484@fozina.com,Huutho06.vn`
 
-        for (let i = 0; i < accountCount; i++) {
+        const accounts = accountStr.split('\n')
+        let i = 0;
+        // for (let i = 0; i < accounts.length; i++) {
+        for (const account of accounts) {
             try {
-                const account = await mailjs.createOneAccount();
-                const email = account.data.username;
-                const password = account.data.password;
+                // const account = await mailjs.createOneAccount();
+                const [email, password] = account.split(',')
+                // const email = account.data.username;
+                // const password = account.data.password;
                 const name = email;
                 if (email === undefined) {
                     i--;
@@ -104,7 +139,7 @@ async function manageMailAndRegister() {
                 logger(`Error with account #${i + 1}: ${error.message}`, 'error');
                 await new Promise(resolve => setTimeout(resolve, 1000)); 
             }
-            
+            i++
         }
     } catch (error) {
         logger(`Error: ${error.message}`, 'error');
